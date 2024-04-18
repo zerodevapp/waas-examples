a3a80ad048146524dcc67c73c288077901d0f42f
jhw <justin84602@gmail.com>
2024-04-18 10:58:31 +0800

    chore: update waas version

--- eoa/src/components/SmartAccountBlock.tsx
+++ eoa/src/components/SmartAccountBlock.tsx
@@ -1,30 +1,40 @@
 "use client";
-import { useKernelClient, useSendUserOperation } from "@zerodev/waas";
+import { useKernelClient, useSendUserOperation, useBalance } from "@zerodev/waas";
 import { Button, Title } from "@mantine/core";
 import { parseAbi } from "viem";
 
 export default function SmartAccountBlock() {
   const { address } = useKernelClient();
-  const { data, write, isPending } = useSendUserOperation();
-  const nftAddress = "0x34bE7f35132E97915633BC1fc020364EA5134863";
-  const abi = parseAbi(["function mint(address _to) public"]);
+  const { data: hash, write, isPending, error } = useSendUserOperation({
+    paymaster: {
+      type: "SPONSOR"
+    }
+  });
+  const tokenAddress = "0x3870419Ba2BBf0127060bCB37f69A1b1C090992B";
+  const abi = parseAbi(["function mint(address _to, uint256 amount) public"]);
+  const { data } = useBalance();
 
   return (
     <>
       <Title order={3}>Smart Account</Title>
       <div className="mb-4">Address: {address}</div>
+      {data && (
+        <div className="mb-4">
+          Balance: {`${data.formatted} ${data.symbol}`}
+        </div>
+      )}
       <div className="flex flex-row justify-center items-center space-x-4 mt-4">
         <Button
           variant="outline"
-          disabled={isPending || !write}
+          disabled={isPending}
           loading={isPending}
           onClick={() => {
             write([
               {
-                address: nftAddress,
+                address: tokenAddress,
                 abi: abi,
                 functionName: "mint",
-                args: [address],
+                args: [address, 1],
                 value: 0n,
               }
             ]);
@@ -33,7 +43,7 @@ export default function SmartAccountBlock() {
           Mint
         </Button>
       </div>
-      {data && <div className="mt-4">UserOp Hash: {data}</div>}
+      {hash && <div className="mt-4">UserOp Hash: {hash}</div>}
     </>
   );
 }
